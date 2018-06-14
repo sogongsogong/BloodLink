9475d1ff278759bf440ac599f187d0cf39383b6c
Sky-Titan <39144048+Sky-Titan@users.noreply.github.com>
2018-06-14 18:06:17 +0900

    MI application first

---
+++ Java/SoftwareEngineering/src/Certification.java
@@ -0,0 +1,50 @@
+
+public class Certification {
+	//«Â«Ê¡ı∞¥√º
+	private String name,email,date,state,phoneNumber;//«Â«˜¡ı¡§∫∏
+	
+	public Certification()
+	{
+		
+	}
+	public Certification(String name,String email,String phoneNumber,String date,String state) 
+	{
+		this.name=name;
+		this.email=email;
+		this.phoneNumber=phoneNumber;
+		this.date=date;
+		this.state=state;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+
+}
